39b130051c20c4730f958a3cd914c24373a1ebf2
Md Shimul Molla <shimuliithost@gmail.com>
2025-07-14 10:53:58 +0600

    Cors set in this project

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,11 @@
 import express, { Application, Request, Response } from 'express';
 import { bookRoutes } from './app/controllers/book.controller';
 import { borrowRoutes } from './app/controllers/borrow.controller';
-
+var cors = require('cors')
 const app: Application = express();
 
 app.use(express.json())
+app.use(cors({origin: "http://localhost:5173"}))
 
 app.use('/api/books', bookRoutes);
 app.use('/api/borrow', borrowRoutes);
