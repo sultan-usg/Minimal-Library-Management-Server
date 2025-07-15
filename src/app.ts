40c6f5d70105d9944b2c2da00057d7dfdda85a66
Md Shimul Molla <shimuliithost@gmail.com>
2025-07-15 11:17:20 +0600

    Client site live link added in cors

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,7 @@ import cors from 'cors';
 const app: Application = express();
 
 app.use(express.json())
-app.use(cors({origin: "http://localhost:5173"}))
+app.use(cors({ origin: ["http://localhost:5173", "https://books-finder-bd.vercel.app"] }))
 
 app.use('/api/books', bookRoutes);
 app.use('/api/borrow', borrowRoutes);
